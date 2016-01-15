748c77ea10a4bd09f172057e69d4cf39aa40e1bf
Deepak Kaul <Deepak.Kaul@nasa.gov>
2016-01-15 11:01:53 -0500

    since unsigned removing error associated with signed

--- sdnv.go
+++ sdnv.go
@@ -5,37 +5,33 @@ import (
 )
 
 type Sdnv struct {
-	Value int64
+	Value uint64
 }
 
-func (s Sdnv) Marshal() ([]byte, error) {
+func (s Sdnv) Marshal() []byte {
 	data := []byte{}
-	if s.Value >= 0 {
-		flag := byte(0)
-		done := false
-		for done == false {
-			newbits := byte(s.Value & 0x7f)
-			s.Value = s.Value >> 7
-			newbyte := byte(newbits + flag)
-			data = append([]byte{newbyte}, data...)
-			if flag == 0 {
-				flag = 0x80
-			}
-			if s.Value == 0 {
-				done = true
-			}
+	flag := byte(0)
+	done := false
+	for done == false {
+		newbits := byte(s.Value & 0x7f)
+		s.Value = s.Value >> 7
+		newbyte := byte(newbits + flag)
+		data = append([]byte{newbyte}, data...)
+		if flag == 0 {
+			flag = 0x80
 		}
-	} else {
-		return nil, errors.New("Invalid input value < 0")
+		if s.Value == 0 {
+			done = true
+		}
 	}
-	return data, nil
+	return data
 }
 
 func (s *Sdnv) Unmarshal(data []byte) error {
-	s.Value = int64(0)
+	s.Value = uint64(0)
 	for i := 0; i < len(data); i++ {
 		s.Value = s.Value << 7
-		s.Value = s.Value + int64(data[i]&0x7f)
+		s.Value = s.Value + uint64(data[i]&0x7f)
 		if (data[i] >> 7) == 0 {
 			break
 		} else if i == (len(data) - 1) {
--- sdnv_test.go
+++ sdnv_test.go
@@ -9,38 +9,24 @@ func TestMarshal(t *testing.T) {
 	values := []sdnv.Sdnv{{0xabc}, {0x1234}, {0x4234}, {0x7f}}
 	answers := [][]byte{{0x95, 0x3c}, {0xa4, 0x34}, {0x81, 0x84, 0x34}, {0x7f}}
 	for i := 0; i < len(values); i++ {
-		encoded, err := values[i].Marshal()
-		if err != nil {
-			t.Error(err)
-		} else {
-			if equal(encoded, answers[i]) == false {
-				t.Error("expected encoded value", encoded, "==", answers)
-			}
+		encoded := values[i].Marshal()
+		if equal(encoded, answers[i]) == false {
+			t.Error("expected encoded value", encoded, "==", answers)
 		}
-	}
-
-	val := sdnv.Sdnv{-1}
-	_, err := val.Marshal()
-	if err == nil {
-		t.Error("expected marshal error but received none")
 	}
 }
 
 func TestUnmarshal(t *testing.T) {
 	values := []sdnv.Sdnv{{0xabc}, {0x1234}, {0x4234}, {0x7f}}
 	for i := 0; i < len(values); i++ {
-		encoded, err := values[i].Marshal()
+		encoded := values[i].Marshal()
+		ans := sdnv.Sdnv{0}
+		err := ans.Unmarshal(encoded)
 		if err != nil {
 			t.Error(err)
 		} else {
-			ans := sdnv.Sdnv{0}
-			err := ans.Unmarshal(encoded)
-			if err != nil {
-				t.Error(err)
-			} else {
-				if ans.Value != values[i].Value {
-					t.Error("expected unmarshal value", ans.Value, "==", values[i].Value)
-				}
+			if ans.Value != values[i].Value {
+				t.Error("expected unmarshal value", ans.Value, "==", values[i].Value)
 			}
 		}
 	}
